2227a0b544a02316609fa42d016a7fe813da8da9
Xavier Martin <xavier.martin.rec@gmail.com>
2025-08-04 20:51:16 +0200

    correction redirection page profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,19 @@
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import { useState } from 'react';
-import { useEffect } from 'react';
 import AccountCard from '../../components/AccountCard'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUserDatas, updateName } from '../../api/api';
 import { useSelector, useDispatch } from "react-redux";
 import { getUserToken, getFirstName, getLastName, isUserLogged, getEmail } from "../../app/selectors";
-import { getUserDatas, updateName } from '../../api/api';
-import './profile.css'
 import { setUserDatas } from '../../features/profileSlice'
+import './profile.css'
 
 
 export default function User() {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const [isPageOnError, setIsPageOnError] = useState(false)
     const [pageErrorMessage, setPageErrorMessage] = useState("")
@@ -27,8 +28,17 @@ export default function User() {
     const userLogged = useSelector(isUserLogged)
     const email = useSelector(getEmail)
 
+
+    //////////////////////////////////////////////// REDIRECTION SI USER NON CONNECTÉ
     useEffect(() => {
-        //////////////////////////////////////////////// RÉCUPÉRATION DES DATAS USER QUAND REMEMBER ME NON COCHÉ
+        if (!userLogged) {
+            navigate("sign-in")
+        } 
+    }, [userLogged, navigate])
+
+
+    //////////////////////////////////////////////// RÉCUPÉRATION DATAS USER QUAND REMEMBER ME NON COCHÉ
+    useEffect(() => {
         if (userLogged && email === "") {
             getUserDatas(userToken)
                 .then((userDatas) => {
@@ -47,13 +57,14 @@ export default function User() {
         }
     }, [dispatch, userLogged, userToken, email])
 
-    //////////////////////////////////////////////// RÉCUPÉRATION DES DATAS USER QUAND REMEMBER ME NON COCHÉ
+
+    //////////////////////////////////////////////// CHARGEMENT INITIAL DU STATE FORMDATA
     useEffect(() => {
         setFormData({firstName, lastName})
     }, [firstName, lastName])
 
 
-    //////////////////////////////////////// SOUMISSION FORM EDIT NAME
+    //////////////////////////////////////// SOUMISSION FORMULAIRE EDIT NAME
     const handleSubmit = (e) => {
         e.preventDefault();
         updateName(userToken, formData)
@@ -70,11 +81,14 @@ export default function User() {
             email: email}))
     };
 
+    
     //////////////////////////////////////// RECUP VALEUR INPUT DANS STATE
     const handleChange = (e) => {
-        const {name, value } = e.target
+        const { name, value } = e.target
         setFormData({...formData, [name]: value})
     }
+
+
 
     return (
 
@@ -86,19 +100,23 @@ export default function User() {
 
                 <div className='userNameWrapper'>
 
+                    {/* TITRE AFFICHÉ SI EDITION DESACTIVÉE */}
                     <h1>
                         Welcome back<br />
                         {!isEditEnabled && firstName + " " + lastName + " !"}
                     </h1>
 
+                    {/* MESSAGE SI PAGE EN ERREUR */}
                     {isPageOnError &&
                         <span className='userPageError'>
                             {pageErrorMessage}
                         </span>
                     }
 
+                    {/* FORMULAIRE AFFICHÉ SI EDITION ACTIVE */}
                     {isEditEnabled &&
                         <div className='editNameForm'>
+
                             {isEditOnError &&
                                 <span className='editError'>
                                     {editErrorMessage}
@@ -127,6 +145,7 @@ export default function User() {
                             </div>
                         </div>
                     }
+                    {/* BOUTON AFFICHÉ SI EDITION DESACTIVÉE */}
                     {!isEditEnabled &&
                         <button onClick={() => {setIsEditEnable(true)} } className="greenButton editButton" >Edit Name</button>
                     }
